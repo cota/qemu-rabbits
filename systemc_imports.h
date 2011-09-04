1454bfe7da2ba610314fc61b5a4649eda451f5c3
Emilio G. Cota <cota@braap.org>
2011-09-04 01:48:14 -0400

    qemu_imports: remove obsolete mem_exclusive fields
    The memex helpers have superseded them; remove them.
    
    Signed-off-by: Emilio G. Cota <cota@braap.org>

--- systemc_imports.h
+++ systemc_imports.h
@@ -19,9 +19,6 @@ typedef unsigned long long  (*systemc_qemu_get_time_fc_t) (void);
 typedef unsigned char   *(*systemc_get_mem_addr_fc_t) (void *sc_obj,
                             unsigned long addr);
 typedef unsigned long   (*systemc_qemu_get_crt_thread_fc_t) (void *qemu_instance);
-typedef void            (*memory_mark_exclusive_fc_t) (int cpu, unsigned long addr);
-typedef int             (*memory_test_exclusive_fc_t) (int cpu, unsigned long addr);
-typedef void            (*memory_clear_exclusive_fc_t) (int cpu, unsigned long addr);
 typedef void            (*wait_wb_empty_fc_t) (void *sc_obj);
 
 struct systemc_import_t
@@ -34,9 +31,6 @@ struct systemc_import_t
     systemc_qemu_get_time_fc_t                      systemc_qemu_get_time;
     systemc_get_mem_addr_fc_t                       systemc_get_mem_addr;
     systemc_qemu_get_crt_thread_fc_t                systemc_qemu_get_crt_thread;
-    memory_mark_exclusive_fc_t                      memory_mark_exclusive;
-    memory_test_exclusive_fc_t                      memory_test_exclusive;
-    memory_clear_exclusive_fc_t                     memory_clear_exclusive;
     wait_wb_empty_fc_t                              wait_wb_empty;
 
     //for log
