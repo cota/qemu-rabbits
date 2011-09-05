9704fceb7f1f5b584c9d4c3da6c8279188e3f46d
Emilio G. Cota <cota@braap.org>
2011-09-05 04:48:20 -0400

    systemc_imports: add function to access a CPU's cycle count
    This matches the recent addition in the qemu_wrapper systemc module.
    
    Signed-off-by: Emilio G. Cota <cota@braap.org>

--- systemc_imports.h
+++ systemc_imports.h
@@ -16,6 +16,7 @@ typedef void            (*systemc_qemu_write_memory_fc_t) (void *sc_obj,
                             unsigned long address, unsigned long data,
                             unsigned char nbytes, int bIO);
 typedef unsigned long long  (*systemc_qemu_get_time_fc_t) (void);
+typedef unsigned long long  (*systemc_qemu_get_no_cycles_fc_t) (void *sc_obj);
 typedef unsigned char   *(*systemc_get_mem_addr_fc_t) (void *sc_obj,
                             unsigned long addr);
 typedef unsigned long   (*systemc_qemu_get_crt_thread_fc_t) (void *qemu_instance);
@@ -29,6 +30,7 @@ struct systemc_import_t
     systemc_qemu_read_memory_fc_t                   systemc_qemu_read_memory;
     systemc_qemu_write_memory_fc_t                  systemc_qemu_write_memory;
     systemc_qemu_get_time_fc_t                      systemc_qemu_get_time;
+    systemc_qemu_get_no_cycles_fc_t                 systemc_qemu_get_no_cycles;
     systemc_get_mem_addr_fc_t                       systemc_get_mem_addr;
     systemc_qemu_get_crt_thread_fc_t                systemc_qemu_get_crt_thread;
     wait_wb_empty_fc_t                              wait_wb_empty;
